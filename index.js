b9b45179f766f1af4313116c13ccc89c51c8b4b2
Suprity08 <84218936+Suprity08@users.noreply.github.com>
2022-05-26 07:44:11 +0530

    update

--- index.js
+++ index.js
@@ -22,31 +22,31 @@ function keyboard(Event)
 function sound(key)
 {
     switch (key) {
-        case "w": var audio = new Audio('sounds/crash.mp3');
+        case "w": var audio = new Audio('crash.mp3');
                   audio.play();
             break;
          
-         case "a": var audio = new Audio('sounds/kick-bass.mp3');
+         case "a": var audio = new Audio('kick-bass.mp3');
             audio.play();
             break;
  
-         case "s": var audio = new Audio('sounds/snare.mp3');
+         case "s": var audio = new Audio('snare.mp3');
             audio.play();
             break;
  
-         case "d": var audio = new Audio('sounds/tom-1.mp3');
+         case "d": var audio = new Audio('tom-1.mp3');
             audio.play();
             break;
  
-         case "j": var audio = new Audio('sounds/tom-2.mp3');
+         case "j": var audio = new Audio('tom-2.mp3');
             audio.play();
             break;
  
-         case "k": var audio = new Audio('sounds/tom-3.mp3');
+         case "k": var audio = new Audio('tom-3.mp3');
             audio.play();
             break;
  
-         case "l": var audio = new Audio('sounds/tom-4.mp3');
+         case "l": var audio = new Audio('tom-4.mp3');
             audio.play();
             break;
  
